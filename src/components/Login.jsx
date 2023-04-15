2519c731df395206fb702186e1f450fb394ed2f7
mominul <ulhouqemomin39@gmail.com>
2023-04-15 15:30:44 +0600

    updated login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,49 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState();
 
-    
+  const handleForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    if(!/(?=.*[a-z])/.test(password)){
+        setError('At least one lowerCase');
+        return;
+    }
 
 
-
+  };
 
   return (
     <div className="mt-5  mx-auto w-50 bg-light p-5 border rounded">
-      <h5 className="text-primary text-center mb-5 fw-bold ">Please Login !!!</h5>
-      <Form>
+      <h5 className="text-primary text-center mb-5 fw-bold ">
+        Please Login !!!
+      </h5>
+      <Form onSubmit={handleForm}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-         
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Remeber" />

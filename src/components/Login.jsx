4cdd3ad06c89242bf6e007e7e9e96414ddcd8fe3
mominul <ulhouqemomin39@gmail.com>
2023-04-15 10:54:50 +0600

    added login pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
-    return (
-        <div>
-            <h5>This is Login</h5>
-        </div>
-    );
+
+    
+
+
+
+
+  return (
+    <div className="mt-5  mx-auto w-50 bg-light p-5 border rounded">
+      <h5 className="text-primary text-center mb-5 fw-bold ">Please Login !!!</h5>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+         
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remeber" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,43 +5,74 @@ import app from "../firebase/firebase-auth";
 const auth = getAuth(app);
 
 const Register = () => {
-  const [email, setEmail] = useState();
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('')
 
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSuccess('');
+    setError('')
     const email = event.target.email.value;
     const password = event.target.password.value;
+    // validation
+    if(!/(?=.*[A-Z])/.test(password)){
+      setError('At least one character is UpperCase')
+      return;
+    }
+    else if(!/(?=.*\d)/.test(password)){
+      setError('At least one Number')
+      return;
+    }
+    else if(!/(?=.*[a-z])/.test(password)){
+      setError('At least one LowerCase')
+      return
+    }
+    else if(!/(?=.*[@$!%*?&])/.test(password)){
+      setError('At least one Special Character')
+      return;
+    }
+    else if(!/$/.test(password)){
+      setError('At least one string')
+      return;
+    }
+
 
     console.log(email, password);
     createUserWithEmailAndPassword(auth, email, password)
     .then(result => {
       const user = result.user;
+      setError('');
       console.log(user);
+      event.target.reset();
+      setSuccess('User has Created Succesfully')
     })
     .catch(error =>{
-      console.error(error);
+      console.error(error.message);
+      setError(error.message);
+      
     })
   };
 
   const handlePass = (event) => {
-    console.log(event.target.value);
-    setEmail(event.target.value);
+    // console.log(event.target.value);
+    // setEmail(event.target.value);
   };
 
   const handleEmailChange = (event) => {
-    console.log(event.target.value);
-    setEmail(event.target.value);
+    // console.log(event.target.value);
+    // setEmail(event.target.value);
   };
   return (
     <div className="w-75 mx-auto text-center mt-5">
       <h5 className="mb-5">Please Register </h5>
+
       <form onSubmit={handleSubmit}>
         <input className="mb-3 p-3 border"
            onChange={handleEmailChange}
           type="email"
           name="email"
-          id=""
+          id="" required
           placeholder="Your Email"
         />
         <br />
@@ -49,12 +80,14 @@ const Register = () => {
           onBlur={handlePass}
           type="password"
           name="password"
-          id=""
+          id="" required
           placeholder="Your Password"
         />{" "}
         <br />
-        <input className="mb-3 btn btn-primary" type="submit" value="register" />
+        <input className="mb-3 btn btn-primary" type="submit"  value="register" />
       </form>
+        <p className="text-danger text-center">{error}</p>
+        <p className="text-success">{success}</p>
     </div>
   );
 };
